f6cf4e7981b096a11daf4f3ad39b54e84f5f8034
Joan Diaz <jdiaz@newrelic.com>
2024-02-03 20:22:21 +0100

    Adding docs and tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,10 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
   """
   def create_deck do
     values = ["Ace", "Two", "Three","Four","Five","Six","Seven","Eight","Nine","Ten","J","Queen","King"]
@@ -11,23 +15,54 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Returns the same deck but randomly shuffled.
+  """
   def shuffle (deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card.
+
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ten of Diamonds")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck,card)
   end
 
+@doc """
+  Devices a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+    iex> deck = Cards.create_deck()
+    iex> {hand, _rest_deck} = Cards.deal(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+
+"""
   def deal(deck, hand_size) do
     Enum.split(deck,hand_size)
   end
 
+  @doc """
+    Save the deck in a file on the path `filename`.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Load the deck on the path `filename`.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok,binary} -> :erlang.binary_to_term(binary)
@@ -35,6 +70,10 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Create a new hand as a list of strings, shuffle it and deal the cards as `hand_size`.
+
+  """
   def create_hand(hand_size) do
     Cards.create_deck()
     |> Cards.shuffle()
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,20 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
-  end
+test "create_deck makes 52 cards" do
+  deck_length = length(Cards.create_deck)
+  assert deck_length == 52
+end
+
+test "shuffling a deck randomizes it" do
+  deck = Cards.create_deck()
+  refute deck == Cards.shuffle(deck)
+end
+
+test "Deal a hand of two cards" do
+  {hand, _rest_deck} = Cards.create_deck() |> Cards.deal(2)
+  assert length(hand) == 2
+
+end
+
 end
